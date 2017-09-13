bc33c7787464f00a23a9dbeeb4bc9da8c43f45a7
Alexey Gladyshev <alexey0gladyshev@gmail.com>
2017-09-13 19:42:27 +0300

    add links to foursquare lists

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-$(document).foundation()
+$(document).foundation();
+
+var lists = [
+	{
+		title: "Yerevan",
+		id: "59b9524b95a722030778d8ed"
+	},
+	{
+		title: "Armenia",
+		id: "59b957aa3731ee2ab6c2677d"
+	}];
