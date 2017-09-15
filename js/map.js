2fc10c132362d557d678a05a9582422d0f1a6453
Alexey Gladyshev <alexey0gladyshev@gmail.com>
2017-09-15 17:24:44 +0300

    add style to map

--- js/map.js
+++ js/map.js
@@ -4,10 +4,94 @@ var infowindow;
 
 var viewMap = {
 	init: function() {
+		var styles = [
+		{
+			"featureType": "administrative",
+			"elementType": "all",
+			"stylers": [
+				{
+					"visibility": "simplified"
+				}
+			]
+		},
+		{
+			"featureType": "landscape",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"visibility": "simplified"
+				},
+				{
+					"color": "#fcfcfc"
+				}
+			]
+		},
+		{
+			"featureType": "poi",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"visibility": "simplified"
+				},
+				{
+					"color": "#fcfcfc"
+				}
+			]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"visibility": "simplified"
+				},
+				{
+					"color": "#dddddd"
+				}
+			]
+		},
+		{
+			"featureType": "road.arterial",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"visibility": "simplified"
+				},
+				{
+					"color": "#dddddd"
+				}
+			]
+		},
+		{
+			"featureType": "road.local",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"visibility": "simplified"
+				},
+				{
+					"color": "#eeeeee"
+				}
+			]
+		},
+		{
+			"featureType": "water",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"visibility": "simplified"
+				},
+				{
+					"color": "#dddddd"
+				}
+			]
+		}
+	]
 		
 		map = new google.maps.Map(document.getElementById('map'), {
 			center: {lat: 40.179186, lng: 44.499103},
 			zoom: 12,
+			styles: styles,
 			disableDefaultUI: true
 
 		});
@@ -29,7 +113,7 @@ var viewMap = {
 		};
 		var infoDiv = '<div>' + location.name +'</div>';
 		infowindow = new google.maps.InfoWindow({
-		    content: infoDiv
+			content: infoDiv
 		  });
 		infowindow.open(map, location.marker);
 	},
