7c81ca152f599f386565af09ce3d6d3be5e08b81
Alexey Gladyshev <alexey0gladyshev@gmail.com>
2017-09-19 16:38:38 +0300

    change ViewModel structure, store current list in localStorage

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -18,19 +18,20 @@ var List = function(data) {
 	this.title = data.title;
 	this.id = data.id;
 	this.locations = ko.observableArray([]);
-	this.categories = ko.computed(function() {
-		var list = [];
-		var index = [];
-		this.locations().forEach(function(location) {
-			location.categories().forEach(function(category) {
-				if (index.indexOf(category.id) === - 1) {
-					list.push(category);
-					index.push(category.id);
-				}		
-			});
-		});
-		return list;
-	}, this);
+	// this.categories = ko.computed(function() {
+	// 	var list = [];
+	// 	var index = [];
+	// 	this.locations().forEach(function(location) {
+	// 		location.categories().forEach(function(category) {
+	// 			if (index.indexOf(category.id) === - 1) {
+	// 				list.push(category);
+	// 				index.push(category.id);
+	// 			}		
+	// 		});
+	// 	});
+	// 	return list;
+	// }, this);
+	this.categories = ko.observableArray([]);
 };
 
 
@@ -81,137 +82,207 @@ var Location = function(data) {
 var ViewModel = function() {
 	var self = this;
 
-	this.currentPosition = ko.observable();
-
-	this.travelModes = ko.observableArray([
+	self.travelModes = ko.observableArray([
 			{title: "Walking", mode: "WALKING"},
 			{title: "Driving", mode: "DRIVING"},
 			{title: "Transit", mode: "TRANSIT"},
 			{title: "Cycling", mode: "BICYCLING"}
 		]);
 
-	this.selectedLocation = ko.observable();
-
-	this.gpsStatus = ko.observable(false);
-
-	this.currentTravelMode = ko.observable( this.travelModes()[0] );
-
-	this.lists = ko.observableArray([]);
-
-	this.listOrders = ko.observableArray([
+	self.listOrders = ko.observableArray([
 		{title: "Order by rating", mode: 'rating', direction: 'DESC'},
 		{title: "Order by duration", mode: 'duration', direction: 'ASC'},
 		{title: "Order by distance", mode: 'distance', direction: 'ASC'}
 		]);
 
-	this.currentOrder = ko.observable( this.listOrders()[0] );
 
-	initialLists.forEach(function(list) {
-		self.lists.push( new List(list) );
-	});
+	this.start = function() {
+		self.currentPosition = ko.observable();
+		self.currentPosition.subscribe(function(newPosition) {
+			self.getDistance();
+		});
+		self.gpsStatus = ko.observable(false);
 
-	this.currentList = ko.observable( this.lists()[0] );
+		self.selectedLocation = ko.observable();
+		self.searchQuery = ko.observable();
 
-	self.currentCategories = ko.computed(function() {
-		return self.currentList().categories();
-	}, this);
-	
-	this.activeCategories = ko.computed(function() {
-		var activeCategories = [];
-		self.currentList().categories().forEach(function(category) {
-			if (category.active()) {
-				activeCategories.push(category);
+		self.currentTravelMode = ko.observable( this.travelModes()[0] );
+		self.currentOrder = ko.observable( this.listOrders()[0] );
+
+		self.lists = ko.observableArray([]);
+		self.currentList = ko.observable();
+
+		initialLists.forEach(function(list) {
+			self.lists.push( new List(list) );
+		});
+
+		if (localStorage.currentList) {
+			var local = JSON.parse(localStorage.currentList);
+			self.lists().forEach(function(list) {
+				if (list.id === local.id) {
+					self.setCurrentList(list);
+				};
+			});
+		} else {
+			self.setCurrentList(self.lists()[0]);
+		};
+		self.compute();
+	};
+
+	this.compute = function() {
+		self.currentCategories = ko.computed(function() {
+			if (self.currentList()) {
+				return self.currentList().categories();
 			};
-		});
-		return activeCategories;	
-	}, this);
+		}, this);
 
-	this.sortedLocations = ko.computed(function() {
-		return self.currentList().locations().sort(function (left, right) { 
-			var mode = self.currentOrder().mode;
-			// var direction = self.currentOrder().direction;
-			if (mode !== 'rating' && left[mode]() && right[mode]()) {
-				// Check if value has been already assigned
-				return left[mode]()['value'] == right[mode]()['value'] ? 0 : (left[mode]()['value'] < right[mode]()['value'] ? -1 : 1)
-			} else {
-				return left[mode] == right[mode] ? 0 : (left[mode] > right[mode] ? -1 : 1)
-			}
-		});
-	}, this);
+		self.activeCategories = ko.computed(function() {
+			// console.log(ko.computedContext.isInitial());
+			// if (localStorage.activeCategories && ko.computedContext.isInitial()) {
+			// 	self.setCategoriesFromStorage();
+			// };.
 
-	this.activeLocations = ko.computed(function() {
-		var activeLocations = [];
-		var positions = [];
-		self.sortedLocations().forEach(function(location) {
-			location.marker().setMap(null);
-			location.categories().forEach(function(locationCat) {
-				self.activeCategories().forEach(function(category) {
-					if (category.id === locationCat.id) {
-						activeLocations.push(location);
-						location.marker().setMap(map);
-						positions.push(location.marker().getPosition());
+			if (self.currentList() && self.currentList().categories()) {
+				var activeCategories = [];
+				self.currentList().categories().forEach(function(category) {
+					if (category.active()) {
+						activeCategories.push(category);
 					};
 				});
+				console.log('calc');
+				console.log(activeCategories);
+				// localStorage.activeCategories = JSON.stringify(activeCategories);
+				return activeCategories;
+			};
+		}, this);
+
+		self.sortedLocations = ko.computed(function() {
+			if (self.currentList()){
+				return self.currentList().locations().sort(function (left, right) { 
+					var mode = self.currentOrder().mode;
+					// var direction = self.currentOrder().direction;
+					if (mode !== 'rating' && left[mode]() && right[mode]()) {
+						// Check if value has been already assigned
+						return left[mode]()['value'] == right[mode]()['value'] ? 0 : (left[mode]()['value'] < right[mode]()['value'] ? -1 : 1)
+					} else {
+						return left[mode] == right[mode] ? 0 : (left[mode] > right[mode] ? -1 : 1)
+					}
+				});
+			};
+		}, this);
+
+		self.activeLocations = ko.computed(function() {
+			var activeLocations = [];
+			var positions = [];
+			self.sortedLocations().forEach(function(location) {
+				location.marker().setMap(null);
+				location.categories().forEach(function(locationCat) {
+					self.activeCategories().forEach(function(category) {
+						if (category.id === locationCat.id) {
+							activeLocations.push(location);
+							location.marker().setMap(map);
+							positions.push(location.marker().getPosition());
+						};
+					});
+				});
+			});
+			if (typeof bounds !== 'undefined') {
+				viewMap.fitBounds(positions);
+			};
+			return activeLocations;
+		}, this);
+
+		self.searchResults = ko.computed(function() {
+			var searchItems;
+			var filteredLocations = [];
+			if (self.searchQuery()) {
+				searchItems = self.searchQuery().toLowerCase().split(' ', 3);
+				self.currentList().locations().forEach(function(location) {
+					searchItems.forEach(function(item) {
+						if (location.name.toLowerCase().includes(item)) {
+							filteredLocations.push(location);
+							return;
+						};
+						if (location.address && location.address.toLowerCase().includes(item)) {
+							filteredLocations.push(location);
+							return;
+						};
+						location.categories().forEach(function(category) {
+							if (category.name.toLowerCase().includes(item)) {
+								filteredLocations.push(location)
+								return;
+							};
+						});
+
+					});
+				});
+				if (filteredLocations.length) {
+					return filteredLocations;
+				} else {
+					return null;
+				}
+			}
+		});
+
+	};
+
+	this.setListCategories = function(list) {
+		var listCategories = [];
+		var index = [];
+		list.locations().forEach(function(location) {
+			location.categories().forEach(function(category) {
+				if (index.indexOf(category.id) === - 1) {
+					listCategories.push(category);
+					index.push(category.id);
+				}		
 			});
 		});
-		if (typeof bounds !== 'undefined') {
-			viewMap.fitBounds(positions);
+		list.categories(listCategories);
+		console.log('set');
+		if (localStorage.activeCategories) {
+			self.setCategoriesFromStorage();
 		};
-		return activeLocations;
-	}, this);
+	};
 
-	this.currentPosition.subscribe(function(newPosition) {
+	this.setCategoriesFromStorage = function() {
+		console.log('storage');
+		var active = JSON.parse(localStorage.activeCategories);
+		var activeIDs = [];
+		console.log(active);
+		active.forEach(function(activeLocation) {
+			activeIDs.push(activeLocation.id);
+		});
+		console.log(activeIDs);
+		self.currentList().categories().forEach(function(category) {
+			if (!activeIDs.includes(category.id)) {
+				category.active(false);
+			};
+		});
+		console.log(self.currentList().categories());
+	};
+
+	this.setCurrentList = function (list) {
+		console.log('here');
+		if (self.currentList()) {
+			self.hideMarkers();
+		};
+		self.currentList(list);
+		self.getListData(list);
 		self.getDistance();
-	});
+		localStorage.currentList = JSON.stringify(list);
+		console.log(list);
+		console.log(self.currentList());
+	};
 
+	this.getListData = function (list) {
 
-	this.lists().forEach(function(list, index) {
 		$.getJSON(window.location.href + "api/v1.0/list/"
 				+ list.id, function(data) {
 					data.response.list.listItems.items.forEach(function (location) {
 						list.locations.push( new Location(location.venue) );
-				});
-			});	
-		});
-
-	this.searchQuery = ko.observable();
-
-	this.searchResults = ko.computed(function() {
-		var searchItems;
-		var filteredLocations = [];
-		if (self.searchQuery()) {
-			searchItems = self.searchQuery().toLowerCase().split(' ', 3);
-			self.currentList().locations().forEach(function(location) {
-				searchItems.forEach(function(item) {
-					if (location.name.toLowerCase().includes(item)) {
-						filteredLocations.push(location);
-						return;
-					};
-					if (location.address && location.address.toLowerCase().includes(item)) {
-						filteredLocations.push(location);
-						return;
-					};
-					location.categories().forEach(function(category) {
-						if (category.name.toLowerCase().includes(item)) {
-							filteredLocations.push(location)
-							return;
-						};
-					});
-
-				});
 			});
-			if (filteredLocations.length) {
-				return filteredLocations;
-			} else {
-				return null;
-			}
-		}
-	});
-
-	this.setCurrentList = function (list) {
-		self.hideMarkers();
-		self.currentList(list);
-		self.getDistance();
+			self.setListCategories(list);
+		});
 	};
 
 	this.hideMarkers = function () {
@@ -233,7 +304,15 @@ var ViewModel = function() {
 	};
 
 	this.toggleCategory = function(category) {
+		
 		category.active(!category.active());
+		console.log('toggle');
+		console.log(self.currentList().categories());
+		console.log(self.currentCategories());
+		console.log(self.activeCategories());
+		// var id = self.currentList().id;
+		// localStorage.activeCategories = JSON.stringify({ id: self.activeCategories() });
+
 	};
 	this.toggleGPS = function() {
 		self.gpsStatus(!self.gpsStatus());
@@ -301,6 +380,24 @@ var ViewModel = function() {
 	this.closeDirectionsCallback = function() {
 		self.shouldShowDirections(false);
 	};
+	// this.getUber = function(location) {
+	// 	var link = 'https://m.uber.com/ul/?'
+	// 	'client_id=<CLIENT_ID>'
+	// 	'&action=setPickup&pickup[latitude]=' + location().LatLng.lat +
+	// 	'&pickup[longitude]=' + location().LatLng.lng +
+	// 	'&pickup[nickname]=' + location.name.replace(/[^\x00-\x7F]/g, "")
+	// 	'&pickup[formatted_address]=1455%20Market%20St%2C%20San%20Francisco%2C%20CA%2094103'
+	// 	'&dropoff[latitude]=37.802374&dropoff[longitude]=-122.405818&dropoff'
+	// 	'[nickname]=Coit%20Tower&dropoff[formatted_address]=1%20Telegraph%20Hill%20Blvd%2C%20San%20Francisco%2C%20CA%2094133'
+	// 	'&product_id=a1111c8c-c720-46c3-8534-2fcdd730040d'
+	// 	'&link_text=View%20team%20roster'
+	// 	'&partner_deeplink=partner%3A%2F%2Fteam%2F9383';
+
+	// };
+
+	
+
+	this.start();
 
 };
 
